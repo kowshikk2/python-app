c81dcd7a4035e76a76474a878b16bde2587c2740
kowshikk2 <73486807+kowshikk2@users.noreply.github.com>
2023-11-15 12:44:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-print("hello montra")
+LED_PIN=17
 
-input("Press any key to exit")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(LED_PIN,GPIO.OUT)
+try:
+    while True:
+    GPIO.output(LED_PIN,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN,GPIO.LOW)
+    time.sleep(1)
+except KeyboardInterrupt:    
+GPIO.cleanup()
