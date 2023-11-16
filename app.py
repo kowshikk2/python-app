fa9ec029893c10460b6b45710ba4588e8bbdce25
kowshikk2 <73486807+kowshikk2@users.noreply.github.com>
2023-11-16 11:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ GPIO.setup(LED_PIN,GPIO.OUT)
 try:
     while True:
         GPIO.output(LED_PIN,GPIO.HIGH)
-        time.sleep(0.3)
+        time.sleep(1)
         GPIO.output(LED_PIN,GPIO.LOW)
-        time.sleep(0.3)
+        time.sleep(1)
 except KeyboardInterrupt:    
     GPIO.cleanup()
